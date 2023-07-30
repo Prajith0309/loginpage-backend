6cecce73bc116e1058dd9b2e66360ced38780927
Prajith <pragithprasad12@gmail.com>
2023-07-31 00:52:33 +0530

    cors after commit

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ connection.then(() => {
   }).catch(err => console.log('Connection failed as invalid connection', err));
 
 app.use(express.json())
-app.use(cors())
+//the problem
+app.use(cors(
+  {origin:'https://teal-selkie-2df54c.netlify.app/'}
+))
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
